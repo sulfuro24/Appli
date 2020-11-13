b21cae261ed67422d8e667232b59778fb8d37cd4
Baptiste Machard <21904691@etu.unicaen.fr>
2020-11-13 14:19:48 +0100

    Premier commit import des fichier initiaux

---
+++ automatisation.bat
@@ -0,0 +1,6 @@
+SET MAVEN_HOME=D:\maven
+
+SET PATH=%MAVEN_HOME%\bin;%PATH%
+
+mvn cobertura:cobertura jar:jar
+
---
+++ src/main/java/code/EvaluateurNiveau.java
@@ -0,0 +1,59 @@
+package code;
+
+public class EvaluateurNiveau {
+    
+    private static final int MIN_COURS=0;
+    private static final int MIN_EXAM=0;
+    private static final int MAX_COURS=25;
+    private static final int MAX_EXAM=75;
+    
+    private static final int LIMITE_DC=30;
+    private static final int LIMITE_CB=50;
+    private static final int LIMITE_BA=70;
+    
+    private static boolean estEntier( String valeur ) {
+        try {
+            Integer.valueOf(valeur);
+        }
+        catch( NumberFormatException e ) {
+            return false;
+        }
+        return true;
+    }
+    
+    private static boolean estDansBorne( int valeur , int min , int max ) {
+        return ( valeur >= min && valeur <= max );
+    }
+
+    public static String evaluerNiveau(String cours, String examen) throws ExceptionValeurNonEntiere, ExceptionValeurHorsBorne {
+        // TODO Auto-generated method stub
+        if ( ! estEntier( cours )) {
+            throw new ExceptionValeurNonEntiere();
+        }
+        if ( ! estEntier( examen )) {
+            throw new ExceptionValeurNonEntiere();
+        }
+        // Les valeurs sont entieres 
+        int iCours = Integer.valueOf( cours );
+        if ( ! estDansBorne(iCours, MIN_COURS , MAX_COURS )) {
+            throw new ExceptionValeurHorsBorne();
+        }
+        int iExamen = Integer.valueOf( examen );
+        if ( ! estDansBorne(iExamen, MIN_EXAM , MAX_EXAM )) {
+            throw new ExceptionValeurHorsBorne();
+        }
+        
+        int somme = iCours + iExamen;
+        
+        if ( somme < LIMITE_DC ) {
+            return "D";
+        } else if ( somme < LIMITE_CB ) {
+            return "C";
+        } else if ( somme < LIMITE_BA ) {
+            return "B";
+        }
+        else return "A";
+            
+    }
+
+}
