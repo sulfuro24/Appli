2a25eb4853a2b44cdc03593732059bb92e1c1bc1
Baptiste Machard <21904691@etu.unicaen.fr>
2020-11-27 14:27:31 +0100

    sonar

--- automatisation.bat
+++ automatisation.bat
@@ -2,5 +2,5 @@ SET MAVEN_HOME=D:\maven
 
 SET PATH=%MAVEN_HOME%\bin;%PATH%
 
-mvn cobertura:cobertura jar:jar
+mvn cobertura:cobertura jar:jar -Dcobertura.report.format=xml checkstyle:checkstyle jar:jar
 
---
+++ lancer-sonar-scanner.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.6
+set PATH=%JAVA_HOME%\bin;%JAVA_HOME%
+C:\plateforme_sonar\sonar-scanner\bin\sonar-scanner.bat
